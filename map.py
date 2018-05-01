8e9cd64272dda12940bff7ddd17a0fad95093ae4
unholydeath785 <ivansonofcoul@gmail.com>
2018-04-30 22:35:26 -0700

    added map and stats command

--- map.py
+++ map.py
@@ -9,131 +9,133 @@ from PIL import Image
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "/maps/"
 
 class Map():
-    def __init__(self, mapDirPath, name):
-        self.tileMap = {}
-        self.colorMap = {}
-        self.itemMap = {}
-        self.map = []
-        self.name = name
+	def __init__(self, mapDirPath, name):
+		self.tileMap = {}
+		self.colorMap = {}
+		self.itemMap = {}
+		self.map = []
+		self.name = name
 
-        mapFile = Image.open(dir_path + mapDirPath + "/map.png")
-        colorFile = open(dir_path + mapDirPath + "/colors.yml", "r")
-        itemsFile = open(dir_path + mapDirPath + "/items.yml", "r")
-        tileFile = open(dir_path + mapDirPath + "/tiles.yml", "r")
+		mapFile = Image.open(dir_path + mapDirPath + "/map.png")
+		colorFile = open(dir_path + mapDirPath + "/colors.yml", "r")
+		itemsFile = open(dir_path + mapDirPath + "/items.yml", "r")
+		tileFile = open(dir_path + mapDirPath + "/tiles.yml", "r")
 
-        self.createTileMap(tileFile)
-        self.loadColors(colorFile)
-        self.loadItems(itemsFile)
-        self.loadMap(mapFile)
+		self.createTileMap(tileFile)
+		self.loadColors(colorFile)
+		self.loadItems(itemsFile)
+		self.loadMap(mapFile)
 
-    def serialize(self):
-        return self
+	def serialize(self):
+		return self
 
-    def printName(self):
-        print(figlet_format(self.name, self.font))
+	def printName(self):
+		print(figlet_format(self.name, self.font))
 
-    def createTileMap(self, file):
-        with file as stream:
-            try:
-                data = yaml.load(stream)
-                tiles = data['tiles']
-                if 'extends' in data:
-                    extends = data['extends']
-                    name = data['name']
-                    if not name in extends:
-                        self.createTileMap(open(dir_path + extends + "/tiles.yml", "r"))
-                        self.loadColors(open(dir_path + extends + "/colors.yml", "r"))
-                if 'font' in data:
-                    self.font = data['font']
-                for tile in tiles:
-                    self.tileMap[tile] = tiles[tile]
-            except yaml.YAMLError as exc:
-                print(exc)
+	def createTileMap(self, file):
+		with file as stream:
+			try:
+				data = yaml.load(stream)
+				tiles = data['tiles']
+				if 'extends' in data:
+					extends = data['extends']
+					name = data['name']
+					if not name in extends:
+						self.createTileMap(open(dir_path + extends + "/tiles.yml", "r"))
+						self.loadColors(open(dir_path + extends + "/colors.yml", "r"))
+				if 'font' in data:
+					self.font = data['font']
+				for tile in tiles:
+					self.tileMap[tile] = tiles[tile]
+			except yaml.YAMLError as exc:
+				print(exc)
 
-    def loadColors(self, file):
-        with file as stream:
-            try:
-                colors = yaml.load(stream)['colors']
-                for key in colors:
-                    self.colorMap[key] = colors[key]
-            except yaml.YAMLError as exc:
-                    print(exc)
+	def loadColors(self, file):
+		with file as stream:
+			try:
+				colors = yaml.load(stream)['colors']
+				for key in colors:
+					self.colorMap[key] = colors[key]
+			except yaml.YAMLError as exc:
+					print(exc)
 
-    def loadItems(self, file):
-        with file as stream:
-            try:
-                data = yaml.load(stream)
-                items = data['items']
-                if 'extends' in data:
-                    extends = data['extends']
-                    name = data['name']
-                    if not name in extends:
-                        self.loadItems(open(dir_path + extends + "/items.yml", "r"))
-                for item in items:
-                    self.itemMap[item] = items[item]
-            except yaml.YAMLError as exc:
-                print(exc)
+	def loadItems(self, file):
+		with file as stream:
+			try:
+				data = yaml.load(stream)
+				items = data['items']
+				if 'extends' in data:
+					extends = data['extends']
+					name = data['name']
+					if not name in extends:
+						self.loadItems(open(dir_path + extends + "/items.yml", "r"))
+				for item in items:
+					self.itemMap[item] = items[item]
+			except yaml.YAMLError as exc:
+				print(exc)
 
-    def loadMap(self, file):
-        pixels = file.load()
-        width = file.size[0]
-        height = file.size[1]
-        for y in range(0, height):
-            mapRow = []
-            self.map.append(mapRow)
-            for x in range(0, width):
-                r, g, b, a = pixels[x,y]
-                hexKey = '%02x%02x%02x' % (r, g, b)
-                tile = self.colorMap[hexKey]
-                tileData = self.tileMap[tile]
-                desc = tileData['desc'].strip()
-                collider = tileData['collider']
-                mapTile = MapTile(x, y, desc, collider)
-                if 'items' in tileData:
-                    mapTile.spawnItems(tileData['items'], self.itemMap)
-                mapRow.append(mapTile)
-    
-    def getTile(self, x, y):
-        return self.map[y][x]
+	def loadMap(self, file):
+		pixels = file.load()
+		width = file.size[0]
+		height = file.size[1]
+		for y in range(0, height):
+			mapRow = []
+			self.map.append(mapRow)
+			for x in range(0, width):
+				r, g, b, a = pixels[x,y]
+				hexKey = '%02x%02x%02x' % (r, g, b)
+				tile = self.colorMap[hexKey]
+				tileData = self.tileMap[tile]
+				desc = tileData['desc'].strip()
+				icon = tileData['icon']
+				collider = tileData['collider']
+				mapTile = MapTile(x, y, icon, desc, collider)
+				if 'items' in tileData:
+					mapTile.spawnItems(tileData['items'], self.itemMap)
+				mapRow.append(mapTile)
+	
+	def getTile(self, x, y):
+		return self.map[y][x]
 
-    def canMove(self, x, y):
-        return not self.map[y][x].collider
+	def canMove(self, x, y):
+		return not self.map[y][x].collider
 
 class MapTile():
-    def __init__(self, x, y, desc, isCollider):
-        self.desc = desc
-        self.x = x
-        self.y = y
-        self.collider = isCollider
-        self.items = []
-        self.itemCounts = {}
+	def __init__(self, x, y, icon, desc, isCollider):
+		self.icon = icon
+		self.desc = desc
+		self.x = x
+		self.y = y
+		self.collider = isCollider
+		self.items = []
+		self.itemCounts = {}
 
-    def spawnItems(self, items, itemMap):
-        for item in items:
-            chance = math.floor(random.random() * 100)
-            if (chance <= item['spawnChance']):
-                count = math.floor(random.random() * item['maxCount']) + 1
-                key = item['item']
-                data = itemMap[key]
-                for i in range(0, count):
-                    obj = Item(key, data['desc'])
-                    if key in self.itemCounts:
-                        self.itemCounts[key] += 1
-                    else:
-                        self.itemCounts[key] = 1
-                    self.items.append(obj)
-    
-    def listItems(self):
-        if len(self.items) == 0:
-            print("No items are on this tile")
-            return None
-        print("Items\n-=+=-")
-        for data in self.itemCounts:
-            print("{} x{}".format(capitalize(data), self.itemCounts[data]))
+	def spawnItems(self, items, itemMap):
+		for item in items:
+			chance = math.floor(random.random() * 100)
+			if (chance <= item['spawnChance']):
+				count = math.floor(random.random() * item['maxCount']) + 1
+				key = item['item']
+				data = itemMap[key]
+				for i in range(0, count):
+					obj = Item(key, data['desc'])
+					if key in self.itemCounts:
+						self.itemCounts[key] += 1
+					else:
+						self.itemCounts[key] = 1
+					self.items.append(obj)
+	
+	def listItems(self):
+		if len(self.items) == 0:
+			print("No items are on this tile")
+			return None
+		print("Items\n-=+=-")
+		for data in self.itemCounts:
+			print("{} x{}".format(capitalize(data), self.itemCounts[data]))
 
-    def __str__(self):
-        return "{}".format(self.desc)
+	def __str__(self):
+		return "{}".format(self.desc)
 
 def capitalize(data):
-    data = str(data)
-    return data[0:1].upper() + data[1:len(data)]
+	data = str(data)
+	return data[0:1].upper() + data[1:len(data)]
--- world.py
+++ world.py
@@ -8,246 +8,270 @@ from termcolor import cprint
 import pickle
 
 class World():
-    def __init__ (self):
-        self.player = Player(1,1)
-        self.map = None
-        self.loadStartMap()
+	def __init__ (self):
+		self.player = Player(1,1)
+		self.map = None
+		self.loadStartMap()
 
-    def loadStartMap(self):
-        if (self.player.race.lower() == "human"):
-            error()
-        elif (self.player.race.lower() == "dwarf"):
-            self.map = Map("The_Pit", "The Pit")
-        elif (self.player.race.lower() == "elf"):
-            error()
-        elif (self.player.race.lower() == "troll"):
-            error()
-        elif (self.player.race.lower() == "orc"):
-            error()
-        else:
-            error()
+	def loadStartMap(self):
+		if (self.player.race.lower() == "human"):
+			error()
+		elif (self.player.race.lower() == "dwarf"):
+			self.map = Map("The_Pit", "The Pit")
+		elif (self.player.race.lower() == "elf"):
+			error()
+		elif (self.player.race.lower() == "troll"):
+			error()
+		elif (self.player.race.lower() == "orc"):
+			error()
+		else:
+			error()
 
-    def serialize(self):
-        return self
+	def serialize(self):
+		return self
 
-    def start(self):
-        self.map.printName()
-        while True:
-            tile = self.map.getTile(self.player.x, self.player.y)
-            print(capitalize(tile))
-            command = self.getInput("Enter Command").lower()
-            self.callCommand(command)
-            print("")
-    
-    def getInput(self, str):
-            print(str)
-            return input("⚔️  ").lower()
+	def start(self):
+		self.map.printName()
+		self.commandList([])
+		while True:
+			tile = self.map.getTile(self.player.x, self.player.y)
+			cprint(capitalize(tile.desc + '\n'), 'blue')
+			command = self.getInput("Enter Command").lower()
+			self.callCommand(command)
+			print("")
+	
+	def getInput(self, str):
+			print(str)
+			return input("⚔️  ").lower()
 
-    def callCommand(self, command):
-        args = command.strip().split(" ")
-        if "move" in args[0]:
-            self.commandMove(args)
-        elif "look" in args[0]:
-            self.commandList(args)
-        elif "take" in args[0]:
-            self.commandTake(args)
-        elif "inventory" in args[0]:
-            self.commandInventory(args)
-        elif "drop" in args[0]:
-            self.commandDrop(args)
-        elif "help" in args[0]:
-            self.commandHelp(args)
-        elif "save" in args[0]:
-            self.commandSave(args)
-        elif "exit" in args[0]:
-            self.commandSave(args)
-            self.commandExit(args)
-        else:
-            print("Unrecognized Command")
+	def callCommand(self, command):
+		args = command.strip().split(" ")
+		if "move" in args[0]:
+			self.commandMove(args)
+		elif "look" in args[0]:
+			self.commandList(args)
+		elif "take" in args[0]:
+			self.commandTake(args)
+		elif "inventory" in args[0]:
+			self.commandInventory(args)
+		elif "drop" in args[0]:
+			self.commandDrop(args)
+		elif "help" in args[0]:
+			self.commandHelp(args)
+		elif "save" in args[0]:
+			self.commandSave(args)
+		elif "exit" in args[0]:
+			self.commandSave(args)
+			self.commandExit(args)
+		elif "stats" in args[0]:
+			self.commandStats(args)
+		elif "map" in args[0]:
+			self.commandMap(args)
+		else:
+			print("Unrecognized Command")
 
-    def commandExit(self, args):
-        print("Exiting...")
-        os.system("cls")
-        os.system("clear")
-        sys.exit()
+	def commandExit(self, args):
+		print("Exiting...")
+		os.system("cls")
+		os.system("clear")
+		sys.exit()
 
-    def commandSave(self, args):
-        print("Saving game file...")
-        saveName = self.getInput("Enter save name")
-        with open("saves/{}.dat".format(saveName), 'wb') as file:
-            pickle.dump(self.serialize(), file, protocol=2)
+	def commandMap(self, args):
+		self.map.printName()
+		currentMap = self.map.map
+		for y in range(len(currentMap)):
+			line = ""
+			for x in range(len(currentMap[y])):
+				if x == self.player.x and y == self.player.y:
+					# prints player with a green color on the map
+					line = line + '\33[32m' + self.player.name[0] + '\33[0m'
+				else:
+					line = line + currentMap[y][x].icon
+			print(line)
 
+	def commandSave(self, args):
+		print("Saving game file...")
+		saveName = self.getInput("Enter save name")
+		with open("saves/{}.dat".format(saveName), 'wb') as file:
+			pickle.dump(self.serialize(), file, protocol=2)
 
-    def commandMove(self, args):
-        collision = False
-        direction = ""
-        if len(args) == 1:
-            command = self.getInput("What Direction")
-            collision, direction = self.move(command)
-        else:
-            collision, direction = self.move(args[1])
-            
-        direction = capitalize(direction)
-        if collision:
-            cprint(direction, 'red')
-        else:
-            print(direction)
+	def commandStats(self, args):
+		print(self.player)
 
-    def commandList(self, args):
-        tile = self.map.getTile(self.player.x, self.player.y)
-        tile.listItems()
-    
-    def commandTake(self, args):
-        count = 1
-        tile = self.map.getTile(self.player.x, self.player.y)
-        items = tile.items
-        itemCounts = tile.itemCounts
-        itemStr = ""
+	def commandMove(self, args):
+		collision = False
+		direction = ""
+		if len(args) == 1:
+			command = self.getInput("What Direction")
+			collision, direction = self.move(command)
+		else:
+			collision, direction = self.move(args[1])
+			
+		direction = capitalize(direction)
+		if collision:
+			cprint(direction, 'red')
+		else:
+			print(direction)
+			self.commandList([])
 
-        if len(items) == 0:
-            print("No items on this tile")
-            return None
-        elif len(args) == 1:
-            tile.listItems()
-            print("")
-            itemStr = self.getInput("What Item?").lower()
-            count = int(self.getInput("How many?").lower())
-        else:
-            for i in range(1, len(args)):
-                try:
-                    count = int(args[i].strip())
-                except ValueError:
-                    itemStr += args[i] + " "
+	def commandList(self, args):
+		tile = self.map.getTile(self.player.x, self.player.y)
+		tile.listItems()
+	
+	def commandTake(self, args):
+		count = 1
+		tile = self.map.getTile(self.player.x, self.player.y)
+		items = tile.items
+		itemCounts = tile.itemCounts
+		itemStr = ""
 
+		if len(items) == 0:
+			print("No items on this tile")
+			return None
+		elif len(args) == 1:
+			tile.listItems()
+			print("")
+			itemStr = self.getInput("What Item?").lower()
+			count = int(self.getInput("How many?").lower())
+		else:
+			for i in range(1, len(args)):
+				try:
+					count = int(args[i].strip())
+				except ValueError:
+					itemStr += args[i] + " "
+		itemStr = itemStr.strip()
 
-        i = 0
-        originalCount = count
-        while i < len(items):
-            if items[i].name == itemStr.strip() and count > 0:
-                if items[i].name in self.player.inventoryCount:
-                    if self.player.inventoryCount[items[i].name] < 100:
-                        self.player.inventoryCount[items[i].name] += 1
-                    else: 
-                        print("You have too many {}s".format(items[i].name))
-                        return None
-                else:
-                    self.player.inventoryCount[items[i].name] = 1
-                self.player.inventory.append(items[i])
-                itemCounts[items[i].name] -= 1
-                if itemCounts[items[i].name] == 0:
-                    del itemCounts[items[i].name]
-                items.remove(items[i])
-                i -= 1
-                count -= 1
-            i += 1
+		i = 0
+		originalCount = count
+		maxCount = itemCounts[itemStr]
+		if originalCount > maxCount:
+			originalCount = maxCount
+		while i < len(items):
+			if items[i].name == itemStr.strip() and count > 0:
+				if items[i].name in self.player.inventoryCount:
+					if self.player.inventoryCount[items[i].name] < 100:
+						self.player.inventoryCount[items[i].name] += 1
+					else: 
+						print("You have too many {}s".format(items[i].name))
+						return None
+				else:
+					self.player.inventoryCount[items[i].name] = 1
+				self.player.inventory.append(items[i])
+				itemCounts[items[i].name] -= 1
+				if itemCounts[items[i].name] == 0:
+					del itemCounts[items[i].name]
+				items.remove(items[i])
+				i -= 1
+				count -= 1
+			i += 1
 
-        if originalCount > 1:
-            itemStr = itemStr.strip()
-            itemStr += "s"
+		if originalCount > 1:
+			itemStr = itemStr.strip()
+			itemStr += "s"
 
-        print("Took {} {}".format(originalCount, itemStr))
+		print("Took {} {}".format(originalCount, itemStr))
 
-    def commandDrop(self, args): 
-        count = 1
-        tile = self.map.getTile(self.player.x, self.player.y)
-        items = self.player.inventory
-        itemCounts = self.player.inventoryCount
-        itemStr = ""
-        
-        if len(args) == 1:
-            self.player.displayInventory()
-            print("")
-            itemStr = self.getInput("What Item?").lower()
-            count = int(self.getInput("How many?").lower())
-        else:
-            for i in range(1, len(args)):
-                try:
-                    count = int(args[i].strip())
-                except ValueError:
-                    itemStr += args[i] + " "
+	def commandDrop(self, args): 
+		count = 1
+		tile = self.map.getTile(self.player.x, self.player.y)
+		items = self.player.inventory
+		itemCounts = self.player.inventoryCount
+		itemStr = ""
+		
+		if len(args) == 1:
+			self.player.displayInventory()
+			print("")
+			itemStr = self.getInput("What Item?").lower()
+			count = int(self.getInput("How many?").lower())
+		else:
+			for i in range(1, len(args)):
+				try:
+					count = int(args[i].strip())
+				except ValueError:
+					itemStr += args[i] + " "
 
-        i = 0
-        originalCount = count
-        while (i < len(items)):
-            if items[i].name == itemStr.strip() and count > 0:
-                if items[i].name in tile.itemCounts:
-                    tile.itemCounts[items[i].name] += 1
-                else:
-                    tile.itemCounts[items[i].name] = 1
-                tile.items.append(items[i])
-                itemCounts[items[i].name] -=1
-                if itemCounts[items[i].name] == 0:
-                    del itemCounts[items[i].name]
-                items.remove(items[i])
-                i -= 1
-                count -=1
-            i += 1
+		i = 0
+		originalCount = count
+		while (i < len(items)):
+			if items[i].name == itemStr.strip() and count > 0:
+				if items[i].name in tile.itemCounts:
+					tile.itemCounts[items[i].name] += 1
+				else:
+					tile.itemCounts[items[i].name] = 1
+				tile.items.append(items[i])
+				itemCounts[items[i].name] -=1
+				if itemCounts[items[i].name] == 0:
+					del itemCounts[items[i].name]
+				items.remove(items[i])
+				i -= 1
+				count -=1
+			i += 1
 
-        if originalCount > 1:
-            itemStr += "s"
-        
-        print("Left {} {}".format(originalCount, itemStr))
+		if originalCount > 1:
+			itemStr += "s"
+		
+		print("Left {} {}".format(originalCount, itemStr))
 
-    def commandInventory(self, args):
-        self.player.displayInventory()
-    
-    def commandHelp(self, args):
-        if len(args) == 1:
-            generalHelp = open('./commands/commands.txt', 'r')
-            for line in generalHelp:
-                data = line.split(':')
-                cprint("{}: ".format(data[0].strip().lower()), 'green')
-                print("{}".format(data[1].strip().lower()))
-        else:
-            try:
-                commandHelp = open('./commands/{}.txt'.format(args[1]), 'r')
-                for line in commandHelp:
-                    print(line[0:len(line) - 1])
-            except:
-                print("No extra help for command: {}".format(args[1]))
-            
+	def commandInventory(self, args):
+		self.player.displayInventory()
+	
+	def commandHelp(self, args):
+		if len(args) == 1:
+			generalHelp = open('./commands/commands.txt', 'r')
+			for line in generalHelp:
+				data = line.split(':')
+				cprint("{}: ".format(data[0].strip().lower()), 'green')
+				print("{}".format(data[1].strip().lower()))
+		else:
+			try:
+				commandHelp = open('./commands/{}.txt'.format(args[1]), 'r')
+				for line in commandHelp:
+					print(line[0:len(line) - 1])
+			except:
+				print("No extra help for command: {}".format(args[1]))
+			
 
-    def move(self, dir):
-        x = self.player.x
-        y = self.player.y
-        if dir.startswith("n"):
-            if y == 0:
-                y = len(self.map.map)
-            if self.map.canMove(x, y - 1):
-                self.player.move(x, y - 1)
-                return False, "Moved North"
-            else:
-                return True, self.map.getTile(x, y - 1)
-        elif dir.startswith("e"):
-            if x == len(self.map.map[y]) - 1:
-                x = -1
-            if self.map.canMove(x + 1, y):
-                self.player.move(x + 1, y)
-                return False, "Moved East"
-            else:
-                return True, self.map.getTile(x + 1, y)
-        elif dir.startswith("s"):
-            if y == len(self.map.map) - 1:
-                y = -1
-            if self.map.canMove(x, y + 1):
-                self.player.move(x, y + 1)
-                return False, "Moved South"
-            else:
-                return True, self.map.getTile(x, y + 1)
-        elif dir.startswith("w"):
-            if x == 0:
-                x = len(self.map.map[y])
-            if self.map.canMove(x - 1, y):
-                self.player.move(x - 1, y)
-                return False, "Moved West"
-            else:
-                return True, self.map.getTile(x - 1, y)
-        else:
-            return "Unrecognized Direction"
+	def move(self, dir):
+		x = self.player.x
+		y = self.player.y
+		if dir.startswith("n"):
+			if y == 0:
+				y = len(self.map.map)
+			if self.map.canMove(x, y - 1):
+				self.player.move(x, y - 1)
+				return False, "Moved North"
+			else:
+				return True, self.map.getTile(x, y - 1)
+		elif dir.startswith("e"):
+			if x == len(self.map.map[y]) - 1:
+				x = -1
+			if self.map.canMove(x + 1, y):
+				self.player.move(x + 1, y)
+				return False, "Moved East"
+			else:
+				return True, self.map.getTile(x + 1, y)
+		elif dir.startswith("s"):
+			if y == len(self.map.map) - 1:
+				y = -1
+			if self.map.canMove(x, y + 1):
+				self.player.move(x, y + 1)
+				return False, "Moved South"
+			else:
+				return True, self.map.getTile(x, y + 1)
+		elif dir.startswith("w"):
+			if x == 0:
+				x = len(self.map.map[y])
+			if self.map.canMove(x - 1, y):
+				self.player.move(x - 1, y)
+				return False, "Moved West"
+			else:
+				return True, self.map.getTile(x - 1, y)
+		else:
+			return "Unrecognized Direction"
 
 def error():
-    raise NotImplementedError('Has Not Been Implemented Yet')
+	raise NotImplementedError('Has Not Been Implemented Yet')
 
 def capitalize(data):
-    data = str(data)
-    return data[0:1].upper() + data[1:len(data)]
+	data = str(data)
+	return data[0:1].upper() + data[1:len(data)]
